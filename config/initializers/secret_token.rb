f04dbee822285ae687e77cbbd9c4368efae6f367
Tej Nair <tej.q.nair@gmail.com>
2014-10-29 05:12:57 -0700

    Setup bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PrincetonClubSchedule::Application.config.secret_key_base = 'ce4d4f5f9f71e408c42cd6df2a0e635ca6411caa165dfaf2e83b169fae28a7562a0dc1ece1e248619a816ecb02c6d442bbefcc444eea3923273ed27460ba6826'
